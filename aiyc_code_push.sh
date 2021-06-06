068c4474ec798e376557924e8d7d67d1b6ea03b5
aiyuechuang <1432803776@qq.com>
2021-06-06 09:29:10 +0800

    update

--- aiyc_code_push.sh
+++ aiyc_code_push.sh
@@ -3,6 +3,7 @@ git add .
 git commit . -m "update"
 #git status
 git push
+git remote add upstream https://github.com/aiyuechuang/Git_Study.git
 git fetch upstream
 git checkout main
 git merge --no-edit upstream/main
